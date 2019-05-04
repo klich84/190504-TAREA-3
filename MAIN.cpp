df64f88986c86f68b9809f93b64ccf53bc800cf5
klich84 <47615038+klich84@users.noreply.github.com>
2019-05-04 10:43:28 -0500

    Create MAIN.cpp

---
+++ MAIN.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+#include "190502 DEFINICIONFUNC.h"
+
+
+
+int main()
+{
+	NumerosAleatorios();
+	MostrarTrama1();
+	RondaActualizacion();
+	cipherData();//Cifrado();
+	
+	return 0;
+}
