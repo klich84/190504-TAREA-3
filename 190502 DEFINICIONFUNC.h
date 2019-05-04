fcfc0331bfe26122b751b1bd9322c224a38787e0
klich84 <47615038+klich84@users.noreply.github.com>
2019-05-04 10:44:23 -0500

    Create 190502 DEFINICIONFUNC.h

---
+++ 190502 DEFINICIONFUNC.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+#include<cstdlib>
+#include <ctime>
+#include<cmath>
+
+using namespace std;
+
+void NumerosAleatorios ();
+void MostrarTrama1 ();
+void RondaActualizacion();
+void ActualizandoTrama1();
+void MostrarTrama1Actu();
+void cipherData();//void Cifrado();
